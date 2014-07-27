54ee331b09b884695f58c4d207ba4ccdfef622a0
Max <maxdlr.7@gmail.com>
2014-07-27 16:49:34 -0600

    asdfgh

---
+++ HojadeTrabajo3/src/HojadeTrabajo3/Principal.java
@@ -0,0 +1,11 @@
+package HojadeTrabajo3;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RandomNum hola = new RandomNum();
+		hola.numeroRandom();
+	}
+
+}
