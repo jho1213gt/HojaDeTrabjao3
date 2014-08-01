695e19a81e85bfb7feccc2c95927c50a9891c1c4
jho1213gt <jho1213gt@gmail.com>
2014-07-31 20:42:20 -0600

    Cambio 1.0.8

---
+++ FInall/HDT3/TestRunHDT3/PrincipalTest.java
@@ -0,0 +1,47 @@
+/*
+* Universidad del Valle de Guetmala
+* Miembros: Aaron Giron 13042; Kuk Ho Chung 13279; Max de Leon 13012
+* Clase: Algoritmos y Estructuras de Datos
+* Seccion: 30
+* Codigos Bubble, Quick, Merge e Inserion Srot referenciado por libro citado
+* Referencia: 
+* - Duane A. Bailey. (2007). Java Structures: Data Structructures in Java for the Principled Programmer. Edicion sqrt(7)
+* - Singh N. (2014). Bubble Sort Example by using Java ArrayList. http://www.kriblog.com/j2se/util/various-bubble-sort-example-in-java-using-string-array-arraylist-linked-list-recursive.html
+*/
+
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author GNXN000000
+ */
+public class PrincipalTest {
+    
+    public PrincipalTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of main method, of class Principal.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Principal.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
---
+++ FInall/HDT3/TestRunHDT3/QuickSortTest.java
@@ -0,0 +1,50 @@
+/*
+* Universidad del Valle de Guetmala
+* Miembros: Aaron Giron 13042; Kuk Ho Chung 13279; Max de Leon 13012
+* Clase: Algoritmos y Estructuras de Datos
+* Seccion: 30
+* Codigos Bubble, Quick, Merge e Inserion Srot referenciado por libro citado
+* Referencia: 
+* - Duane A. Bailey. (2007). Java Structures: Data Structructures in Java for the Principled Programmer. Edicion sqrt(7)
+* - Singh N. (2014). Bubble Sort Example by using Java ArrayList. http://www.kriblog.com/j2se/util/various-bubble-sort-example-in-java-using-string-array-arraylist-linked-list-recursive.html
+*/
+
+
+import java.util.ArrayList;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author GNXN000000
+ */
+public class QuickSortTest {
+    
+    public QuickSortTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of quickSort method, of class QuickSort.
+     */
+    @Test
+    public void testQuickSort() {
+        System.out.println("quickSort");
+        ArrayList<Integer> data = new ArrayList<Integer> ();
+        int n = 0;
+        QuickSort.quickSort(data, n);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+}
+
+    
